97453df3bb2bb64ed60f693c3bfa0934825ef455
Caue-MV <cauemaxvilela@gmail.com>
2026-09-05 16:41:04 -0300

    Feat: Correção de alguns erros no sistema de mapeamento de nomes, otimização de certas funcionalidades e código agora está comentado

--- Backend/src/dicionario.js
+++ Backend/src/dicionario.js
@@ -1,15 +1,3 @@
-const instancia = [];                                                   //Armazena itens a serem adicionados
-
-const aliasesToNome = {                                                 //Dicionario de nomes conhecidos
-    "req.": "requeijao",
-    "requeijao": "requeijao",
-    "leite": "leite",
-    "pao": "pao",
-};
-
-const map = new Map(Object.entries(aliasesToNome));
-
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -25,49 +13,66 @@ const askQuestion = (question) => {
     });
 };
 
+const instancia = [];                                                   //Armazena itens a serem adicionados
+
+const aliasesToNome = {                                                 //Dicionario de nomes conhecidos
+    "req.": "requeijao",
+    "requeijao": "requeijao",
+    "leite": "leite",
+    "pao": "pao",
+};
+
+const map = new Map(Object.entries(aliasesToNome));                     //Faz o mapeamento de nomes definidos no dicionario
+
 const main = async () => {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {                                       //Recebe 5 entradas de teste do usuario
         let item = await askQuestion('Adicione um item:\n');
-        item = item.toLowerCase();
+        item = item.toLowerCase();                                      //Converte entrada para lowercase
         let quant;
-        if (/\d+/.test(item)) {
-            quant = item.match(/\d+/g)[0];
-            item = item.replace(/\d+/g, '').trim();
-        }else{
+        if (/\d+/.test(item)) {                                         //Verifica se usuario definiu quantidade de itens a serem adicionados
+            quant = parseInt(item.match(/\d+/g)[0]);
+            item = item.replace(/\d+/g, '').trim();                     //Retira numeros do nome
+        } else {
             item = item.trim();
-            quant = 1;
+            quant = 1;                                                  //Caso não haja quantidade, trata como se houvesse 1 item
         }
 
-        for (const [key, value] of map) {
-            if (item.includes(key)) {
-                item = value;
+        if (!Array.from(map.values()).includes(item) && !Array.from(map.keys()).includes(item)) {   //Verifica se o item está presente no dicionario
+            let novoitem = await askQuestion('Item não encontrado, qual o nome do item?\n');        //Caso não esteja, o adiciona
+            novoitem = novoitem.toLowerCase().trim();
+            if (Array.from(map.keys()).includes(novoitem)) {            //Caso nome inserido corresponda a um dos outros nomes conhecidos de um produto, o substitui
+                for (const [key, value] of map) {                       
+                    if (novoitem.includes(key)) {
+                        novoitem = value;
+                    }
+                }
+            }
+            map.set(item, novoitem);
+            item = novoitem;
+        } else {
+            for (const [key, value] of map) {                           //Substitui nome do dicionario por nome padrao caso esteja presente no dicionario
+                if (item.includes(key)) {
+                    item = value;
+                }
             }
         }
 
-        if (!Array.from(map.values()).includes(item)) {
-            let novoitem = await askQuestion('Item não encontrado, qual o nome do item?\n');
-            novoitem = novoitem.toLowerCase().trim();
-            map.set(item, novoitem);
-            instancia.push({
-                Nome: novoitem,
-                quantidade: quant,
+        if (instancia.some((obj) => obj.Nome === item)) {               //Caso item já esteja na dispensa, atualiza a quantidade a ser adicionada
+            instancia.find((o, i) => {
+                if (o.Nome === item) {
+                    instancia[i].quantidade += quant;
+                    return true;
+                }
             });
         } else {
-            if(instancia.some((obj) => obj.Nome === item)){
-                instancia.find((o, i) =>{
-                    if(o.Nome === item){
-                        instancia[i].quantidade += quant;
-                        return true;
-                    }
-                });
-            }
-            instancia.push({
+            instancia.push({                                            //Caso item não esteja na dispensa, o coloca
                 Nome: item,
                 quantidade: quant,
             });
         }
 
-        instancia.sort((a, b) => a.Nome.localeCompare(b.Nome));
+
+        instancia.sort((a, b) => a.Nome.localeCompare(b.Nome));         //Ordena a dispensa em ordem alfabética
     }
     console.log(instancia);
     rl.close();
