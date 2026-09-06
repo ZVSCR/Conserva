2e9cd5169941da139861b86d30760e590afd660d
Heloiza Coimbra <helocoimbra2013@gmail.com>
2026-09-06 09:50:26 -0300

    Header da Página de Produtos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/PageHome';
 import Login from './pages/Login/PageLogin';
 import Register from './pages/Register/PageRegister';
+import Produtos from './pages/Produtos/PageProd';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
 
         <Route path="/Login" element={<Login />} />
         <Route path="/Registrar" element={<Register />} />
+
+        <Route path='/Produtos' element={<Produtos />} />
 
         {/* Rota de página não encontrada (404) - opcional mas recomendado */}
         <Route path="*" element={<h1>Página não encontrada (404)</h1>} />
---
+++ Frontend/src/pages/Produtos/PageProd.jsx
@@ -0,0 +1,15 @@
+import styles from './Produtos.module.css';
+import Header from './Header';
+
+function PageProd(){
+    return(
+        <>
+            <Header />
+            <main>
+                <h1>Lista de Produtos</h1>
+            </main>
+        </>
+    );
+}
+
+export default PageProd;
