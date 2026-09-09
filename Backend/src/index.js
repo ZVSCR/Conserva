a5f96285f7b6539fe6a57a6483451f94041c15fe
Davi Santos <dssantos.0620@gmail.com>
2026-09-09 15:51:03 -0300

    feat: configuração conexão inicial Express <-> PostgreSQL

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API funcionando'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
