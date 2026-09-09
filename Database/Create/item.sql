7cf0a61f569b959e9a4d377eb435c1033220d4f7
Caue-MV <cauemaxvilela@gmail.com>
2026-09-09 12:20:37 -0300

    Feat: Criada entidade para armazenamento de itens comprados

---
+++ Database/Create/item.sql
@@ -0,0 +1,10 @@
+create table item (
+    id serial,
+    compra_id integer references compra(id),
+    nome_item varchar(100) not null, 
+    quantidade numeric(9, 2) not null,
+    unidade_de_medida varchar(20) not null,
+    valor_unitario numeric(9, 2) not null,
+    validade_estimada date,
+    primary(id, compra_id)
+);  
