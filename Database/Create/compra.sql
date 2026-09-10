d8fbd835b7d56d5e6170137ae1742237bf21a409
Caue-MV <cauemaxvilela@gmail.com>
2026-09-10 12:27:33 -0300

    Feat: Corrigidos erros na definição de chaves primarias

--- Database/Create/compra.sql
+++ Database/Create/compra.sql
@@ -1,8 +1,7 @@
 create table compra (
-    id serial,
-    usuario_id integer references user(id),
+    id serial primary key,
+    usuario_id integer references users(id) on delete cascade not null,
     data_compra timestamp default current_timestamp,
     valor_total numeric(9, 2) not null,
     estabelecimento varchar(50)
-    primary key (id, usuario_id)
 );  
--- Database/Create/item.sql
+++ Database/Create/item.sql
@@ -1,10 +1,10 @@
 create table item (
     id serial,
-    compra_id integer references compra(id),
+    compra_id integer references compra(id) on delete cascade,
     nome_item varchar(100) not null, 
     quantidade numeric(9, 2) not null,
     unidade_de_medida varchar(20) not null,
     valor_unitario numeric(9, 2) not null,
     validade_estimada date,
-    primary key(id, compra_id)
+    primary key (id, compra_id)
 );  
