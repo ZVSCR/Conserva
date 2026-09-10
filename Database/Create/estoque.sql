ac4e01f151ae26dd63bf6fc1250742ab2009786a
Heloiza Coimbra <helocoimbra2013@gmail.com>
2026-09-10 19:34:26 -0300

    PK única. FK de usuário e Item, sendo Item Unique para evitar duplicidade do mesmo item em mais de um estoque

--- Database/Create/estoque.sql
+++ Database/Create/estoque.sql
@@ -1,12 +1,9 @@
-CREATE TABLE EstoqueItem (
-    id,
-    usuario_id INT NOT NULL,
-    item_id INT NOT NULL,
-    quantidade_disponivel INT NOT NULL DEFAULT 1,
-    
-    PRIMARY KEY (id, usuario_id, item_id),
-    
+CREATE TABLE estoque(
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER NOT NULL,
+    item_id INTEGER NOT NULL UNIQUE,
+    quantidade_disponivel NUMERIC(9, 2) NOT NULL,
+
     FOREIGN KEY (usuario_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES item(id),
-       
+    FOREIGN KEY (item_id) REFERENCES item(id) ON DELETE CASCADE
 );
