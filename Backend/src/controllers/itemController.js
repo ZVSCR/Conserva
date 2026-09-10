a2c95faca7ea050b7f5d7fdb87dd4db336359183
gabrieldpbarros <gabrielpanovich@gmail.com>
2026-09-10 16:41:50 -0300

    feat: adiciona tratamento de erro na listagem de itens

--- Backend/src/controllers/itemController.js
+++ Backend/src/controllers/itemController.js
@@ -1,14 +1,31 @@
 const { buscarItens, buscarPorId } = require('../repositories/itemRepository');
 
 async function listarItens(req, res) {
-    const items = await buscarItens();
-    res.json(items);
+    try {
+        const items = await buscarItens();
+        res.json(items);
+    } catch (erro) {
+        res.status(500).json({
+            erro: 'Erro ao buscar itens no banco'
+        });
+    }
 }
 
 async function listarPorId(req, res) {
-    const { id } = req.params;
-    const item = await buscarPorId(id);
-    res.json(item);
+    try {
+        const { id } = req.params;
+        const item = await buscarPorId(id);
+        if (!item) {
+            return res.status(404).json({
+                erro: 'Item não encontrado'
+            });
+        }
+        res.json(item);
+    } catch (erro) {
+        res.status(500).json({
+            erro: 'Erro ao buscar item no banco'
+        });
+    }
 }
 
 module.exports = { listarItens, listarPorId }
