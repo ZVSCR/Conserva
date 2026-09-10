fe4eae7e10fda2a473da31eba86f55b05d83b431
gabrieldpbarros <gabrielpanovich@gmail.com>
2026-09-10 16:40:13 -0300

    fix: remove try-catch indesejado

--- Backend/src/repositories/itemRepository.js
+++ Backend/src/repositories/itemRepository.js
@@ -1,39 +1,29 @@
 const sql = require('../config/database')
 
 async function buscarItens() {
-    try {
-        // será implementado quando houver uma forma de autenticação
-        //const query = await sql`
-        //    SELECT * FROM item
-        //    JOIN compra ON item.compra_id = compra.id
-        //    JOIN users ON compra.usuario.id = users.id
-        //    WHERE users.id = ${usuarioId};
-        //`;
-        const query = await sql`
-            SELECT * FROM item
-            JOIN compra ON item.compra_id = compra.id
-            JOIN users ON compra.usuario_id = users.id;
-        `;
-        const resultado = query[0];
+    // será implementado quando houver uma forma de autenticação
+    //const query = await sql`
+    //    SELECT * FROM item
+    //    JOIN compra ON item.compra_id = compra.id
+    //    JOIN users ON compra.usuario.id = users.id
+    //    WHERE users.id = ${usuarioId};
+    //`;
+    const query = await sql`
+        SELECT * FROM item
+        JOIN compra ON item.compra_id = compra.id
+        JOIN users ON compra.usuario_id = users.id;
+    `;
 
-        return resultado
-    } catch (erro) {
-       return erro;
-    }
+    return query;
 }
 
 async function buscarPorId(id) {
-    try {
-        const query = await sql`
-        SELECT * FROM item
-        WHERE id = ${id};
-        `;
-        const resultado = query[0];
+    const query = await sql`
+    SELECT * FROM item
+    WHERE id = ${id};
+    `;
 
-        return resultado
-    } catch (erro) {
-        return erro;
-    }
+    return query[0];
 }
 
 module.exports = { buscarItens, buscarPorId };
