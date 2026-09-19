145e91b362f777dbafa1dafa6901cdb6c2caba47
Heloiza Coimbra <helocoimbra2013@gmail.com>
2026-09-19 20:24:36 -0300

    Link da logo Conserva indo para home

--- Frontend/src/pages/Estoque/Header.jsx
+++ Frontend/src/pages/Estoque/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from './Estoque.module.css';
 import perfil from '../../assets/IconsEstoque/perfil.png';
 function Header() {
     return (
         <header className={styles.header}>
             <h1 className={styles.logo}>
-                <a href="#">Conserva</a>
+                 <Link to="/home">Conserva</Link>
             </h1>
 
             <nav>
