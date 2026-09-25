3afa0dd67b27b57a430ddcc6408a0a89163e64ab
gabrieldpbarros <gabrielpanovich@gmail.com>
2026-09-25 01:22:15 -0300

    feat: adiciona rotas de visualizacao

--- Backend/src/routes/compraRoutes.js
+++ Backend/src/routes/compraRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const compraRouter = express.Router();
 const {
+    listarCompras,
+    listarItensPorCompra,
     atualizarCompra,
     atualizarItensPorCompra
 } = require('../controllers/compraController');
 
+compraRouter.route('/')
+    .get(listarCompras);
+
 compraRouter.route('/:compraId')
+    .get(listarItensPorCompra)
     .patch(atualizarCompra);
 
 compraRouter.route('/:compraId/items/:itemId')
